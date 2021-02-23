ccd8981899b8218d435291aa422d9e2954eec312
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-22 22:36:05 -0500

    Add SpellChecker module Basic structure Create module files, add header guards, add some basic modules.

---
+++ SpellChecker.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <sstream>
+#include "SpellChecker.h"
+
+namespace sdds {
+
+	SpellChecker::SpellChecker(const char* filename) {
+		ifstream file(filename);
+		if (!file) {
+			throw "Bad file name!";
+		}
+		else {
+			string temp;
+			size_t i = 0;
+			while (file && i < MAX) {
+				getline(file, m_badWords[i], ' ');
+				m_badWords[i].erase(0, m_badWords[i].find_first_not_of(' '));
+				m_badWords[i].erase(m_badWords[i].find_last_not_of(' ') + 1);
+				getline(file, m_goodWords[i], '\n');
+				m_goodWords[i].erase(0, m_goodWords[i].find_first_not_of(' '));
+				m_goodWords[i].erase(m_goodWords[i].find_last_not_of(' ') + 1);
+				i++;
+			}
+			file.close();
+		}
+	}
+
+}
---
+++ SpellChecker.h
@@ -0,0 +1,19 @@
+#ifndef SDDS_SPELLCHECKER_H
+#define SDDS_SPELLCHECKER_H
+
+#include <string>
+
+using namespace std;
+
+const int MAX = 5;
+
+namespace sdds {
+	class SpellChecker {
+		string m_badWords[MAX];
+		string m_goodWords[MAX];
+	public:
+		SpellChecker(const char* filename);
+	};
+}
+
+#endif
